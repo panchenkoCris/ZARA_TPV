7411fd181b217c93649de2b742792522d24c050b
Cristina <ntv29828@educastur.es>
2023-04-22 01:00:53 +0200

    Inserción 3 - Nuevo dialógo

---
+++ app/src/main/java/com/example/zara_tpv/windows/LoginWindow.java
@@ -0,0 +1,16 @@
+package com.example.zara_tpv.windows;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.zara_tpv.R;
+
+public class LoginWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_window);
+    }
+}
