36786da817bd5a310d4b0a497875c5563ea319a4
Cristina <ntv29828@educastur.es>
2023-05-25 23:13:23 +0200

    Inserción 7.2 - Nuevas funcionalidades añadidas

--- app/src/main/java/com/example/zara_tpv/windows/PayWindow.java
+++ app/src/main/java/com/example/zara_tpv/windows/PayWindow.java
@@ -1,4 +1,16 @@
 package com.example.zara_tpv.windows;
 
-public class PayWindow {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.zara_tpv.R;
+
+public class PayWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pay_window);
+    }
 }
